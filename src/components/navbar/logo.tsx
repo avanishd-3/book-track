069d3df9e9e2ec14460e6aa1a2b4051a4c6f5b10
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-06-16 10:27:37 -0700

    Add basic nav menu

---
+++ src/components/navbar/logo.tsx
@@ -0,0 +1,22 @@
+export const Logo = () => (
+  <svg
+    id="logo-7"
+    width="124"
+    height="32"
+    viewBox="0 0 124 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <text
+        x="36"
+        y="22"
+        fontFamily="Arial, Helvetica, sans-serif"
+        fontSize="16"
+        fontWeight="bold"
+        fill="currentColor"
+        className="fill-foreground"
+    >
+        BookTrack
+    </text>
+  </svg>
+);
