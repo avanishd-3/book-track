ad6db02b8b510fbfa5319dd0cf059b50a1d4a25b
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-06-18 16:20:40 -0700

    Reduce margin bottom on about me to look better when inspecting stuff

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function AboutPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-muted">
       <h1 className="text-4xl font-bold mb-4">About Me</h1>
-      <p className="text-lg text-center max-w-xl mb-100">
+      <p className="text-lg text-center max-w-xl mb-50">
         Welcome to my application! I designed this app to learn how to use Drizzle and Better Auth.
       </p>
     </div>
