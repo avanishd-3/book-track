54438268bda28b67c2f1a998d4e3981cc53fa744
Avanish Davuluri <avanishdavuluri@gmail.com>
2025-06-19 09:35:21 -0700

    get started redirects to login

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowUpRight, CirclePlay } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
@@ -16,8 +17,14 @@ const Hero = () => {
             never lose track of your books again.
         </p>
         <div className="mt-12 flex items-center justify-center gap-4">
-          <Button size="lg" className="rounded-full text-base">
-            Get Started <ArrowUpRight className="!h-5 !w-5" />
+          <Button
+           size="lg"
+           className="rounded-full text-base"
+           asChild>
+
+           <Link 
+            href="/login">Get Started <ArrowUpRight className="!h-5 !w-5" />
+           </Link>
           </Button>
           <Button
             variant="outline"
